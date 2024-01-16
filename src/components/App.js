889f6c576c14ee031ce8e78dd3f53fcbbdff58a2
Dipak1121 <dipaks9604@gmail.com>
2024-01-16 12:43:15 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [message, setMessage] = useState("");
+
+  function handleChange(e){
+    setMessage(e.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input type="text"
+        onChange={handleChange}></input>
+
+        {message != "" ? <p>Hello {message}!</p> : ""}
     </div>
   )
 }
